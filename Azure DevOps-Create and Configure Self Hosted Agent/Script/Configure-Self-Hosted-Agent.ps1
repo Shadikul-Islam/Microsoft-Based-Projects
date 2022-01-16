3edfc6bda4afa97249c75988af50f1712e4fca24
shadikul Islam <shadikul.islam.shuvo@gmail.com>
2022-01-17 04:02:30 +0600

    Script Added

---
+++ Azure DevOps-Create and Configure Self Hosted Agent/Script/Configure-Self-Hosted-Agent.ps1
@@ -0,0 +1,4 @@
+mkdir agent ; cd agent
+Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory("$HOME\Downloads\vsts-agent-win-x64-2.196.2.zip", "$PWD")
+
+.\config.cmd

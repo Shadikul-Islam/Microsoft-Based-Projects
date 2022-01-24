65d23055aa2da1999b017c22d1838bd3c059521c
shadikul Islam <shadikul.islam.shuvo@gmail.com>
2022-01-25 00:26:43 +0600

    Create Azure VM Using Powershell Script Added

---
+++ Create Azure VM Using Powershell Script/Script/Create Azure VM Using Powershell Script.ps1
@@ -0,0 +1,10 @@
+﻿New-AzVm `
+    -ResourceGroupName "MyNewResourceGroup" `
+    -Name "MyWindowsVM" `
+    -Location "East US" `
+    -Size "Standard_B4ms" `
+    -VirtualNetworkName "MyVirtualNetwork" `
+    -SubnetName "MySubnet" `
+    -SecurityGroupName "MySecurityGroup" `
+    -PublicIpAddressName "MyWindowsVMPublicIP" `
+    -OpenPorts 80,3389
